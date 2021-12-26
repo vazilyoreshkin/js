9cdf53fefddd9776ae4073bb03440bd449d87696
Vazily <vazily2015@gmail.com>
2021-12-26 20:57:50 +0300

    error

---
+++ src/js/modules/modals.js
@@ -0,0 +1,23 @@
+const modals = () => {
+    function bindModal(trigger, modal, close) {
+        trigger.addEventlistener('click', (e) => {
+            if (e.target) {
+                e.preventDefault();
+            }
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden";
+        });
+        close.addEventlistener('click', () => {
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+        });
+    }
+    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+        modalEngineer = document.querySelector('.popup_engineer'),
+        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+};
+
+
+export default modals;
